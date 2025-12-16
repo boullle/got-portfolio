fb26371a6439805598dff9490a85acb66eb2bdbb
boullle <bill.bouilly1@gmail.com>
2025-12-16 14:57:55 +0100

    finalisation style

--- src/app/shared/services/character.ts
+++ src/app/shared/services/character.ts
@@ -2,15 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import {Characters} from '../models/characters.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  private charactersUrl: string = 'https://thronesapi.com/api/v2/Characters';
+  private charactersUrl: string = environment.apiUrl;
   private httpClient=inject(HttpClient);
+
   getCharacters() :Observable<Characters[]> {
-    return this.httpClient.get<Characters[]>(this.charactersUrl);
+    return this.httpClient.get<Characters[]>(this.charactersUrl + '/Characters');
   }
 }
 
--- src/app/shared/services/continent.ts
+++ src/app/shared/services/continent.ts
@@ -2,15 +2,16 @@ import { inject, Injectable } from '@angular/core';
 import { Continents } from '../models/continents.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContinentService {
-   private continentsUrl: string = 'https://thronesapi.com/api/v2/Continents';
+   private continentsUrl: string = environment.apiUrl;
   private httpClient=inject(HttpClient);
   getAllContinents() :Observable<Continents[]> {
-    return this.httpClient.get<Continents[]>(this.continentsUrl);
+    return this.httpClient.get<Continents[]>(this.continentsUrl + '/Continents');
   }
   
 }
